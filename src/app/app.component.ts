e34041fe91d969d776a232ab787c328b68aa9ee6
Kevin Morales <keviinmoralees132@gmail.com>
2022-02-25 14:43:05 -0500

    Commit1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
-
+import { Employee } from './models/employee'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crudSimple';
+  employeeArray: Employee[] = [
+    {
+      id: 1,
+      name: 'Kevin',
+      country: 'Colombia'
+    },
+    {
+      id: 2,
+      name: 'Daniela',
+      country: 'USA'
+    },
+    {
+      id: 3,
+      name: 'howks',
+      country: 'Russia'
+    },
+  ];
+
+  selectedEmployee: Employee = new Employee();
+
+  addOrEdit(){
+    if(this.selectedEmployee.id === 0){
+      this.selectedEmployee.id = this.employeeArray.length + 1;
+      this.employeeArray.push(this.selectedEmployee);
+    }
+   
+    this.selectedEmployee = new Employee();
+  }
+  onDelete(){
+    if(confirm('Are you sure you want to delete it?')){
+      this.employeeArray = this.employeeArray.filter(x => x != this.selectedEmployee);
+      this.selectedEmployee = new Employee();
+    }
+    
+
+  }
+  openForEdit(employee: Employee){
+    this.selectedEmployee = employee;
+  }
+
+  
 }
